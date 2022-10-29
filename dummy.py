4e3b218d4be9d505ccbd0776865cdd088549f492
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2022-10-29 17:39:20 -0600

    Updated search_artists function

--- dummy.py
+++ dummy.py
@@ -1,17 +1,14 @@
-from operator import truediv
-from pickle import TRUE
 import sqlite3
-from tracemalloc import start
 from datetime import datetime
 import os
 import time
 from getpass import getpass
 
 conn = sqlite3.connect('./temp.db')
-cur	= conn.cursor()
+cur = conn.cursor()
+
 
 def start_session(id):
-
     # Implement Start a session
     print()
     cur.execute("select * from sessions where uid=?", (id,))
@@ -28,7 +25,7 @@ def start_session(id):
         if (i not in store):
             sno = i
             break
-        i = i+1
+        i = i + 1
 
     now = datetime.now()
     current_date = now.date()
@@ -56,22 +53,69 @@ def search_songs_playlists():
                 # print(data)
                 for row in data:
                     print('{}.\tSong ID: {} | Title: {} | Duration: {} seconds'.format(i, row[0], row[1], row[2]))
-                    i+=1
+                    i += 1
 
 
 def search_artists():
-    # Implement Search for artists
+    keyword = input('Search for an artist\'s name: ').strip()
+    cur.execute("with e1 as (Select a.name ename, count(a.name) num from songs s, artists a, perform p where a.aid = p.aid and s.sid = p.sid and (s.title like '%{}%' or a.name like '%{}%') group by ename)".format(keyword, keyword) + " Select a.name, a.nationality, count(s.sid) from songs s, artists a, perform p, e1 where a.aid = p.aid and s.sid = p.sid and e1.ename = a.name group by a.name order by num desc")
+    data = cur.fetchall()
+
+    if len(data) == 0:
+        print('\nSorry! There is no artist with this name')
+
+    i = 0
     print()
+    print(str('Found ' + str(len(data)) + ' matching results (Name, Nationality, Number of Songs)').center(150, '-'))
+
+    for j in data:
+        print(j)
+        i +=1
+        if i == len(data):
+            print('This is end of our search result.'.center(150, '-'))
+
+        if i % 5 == 0:
+            print()
+            UserInput = input('Do you want to continue searching ' + str(len(data) -i) + ' left (Press Y/N) or Select an artist (type name)? ').strip()
+            print()
+            if UserInput.lower() == 'y':
+                continue
+
+            elif UserInput.lower() == 'n':
+                print()
+                print('This is end of our search result.'.center(150, '-'))
+                break
+
+            else:
+                cur.execute("Select s.sid, s.title, s.duration from songs s, artists a, perform p where a.aid = p.aid and s.sid = p.sid and a.name = ?;", (UserInput.title(),))
+                artist_data = cur.fetchall()
+
+                if len(artist_data) == 0:
+                    print('Invalid choice ')
+                    break
+
+                print(str('Songs of ' + UserInput.title() + ' (id, title, duration)').center(150, '-'))
+                for i in artist_data:
+                    print(i)
+
+                print()
+                SongSelection = input('Do you want to select any song - Enter it\'s name: ').strip()
+                # Use this SongSelection variable to perform a song action
+                break
+
+
+
+
+
 
 def user_session(id):
-
     # User Session
     menu = "User Session\n1. Start a session\n2. Search for songs and playlists\n3. Search for artists\n4. End the session"
     while True:
         print(menu)
 
         user_option = input(str("Please enter an option #: "))
-        while(user_option not in ["1", "2", "3", "4"]):
+        while (user_option not in ["1", "2", "3", "4"]):
             clearTerminal()
             print(menu)
             user_option = input(str("Invalid option entered. Please enter an option #: "))
@@ -95,14 +139,16 @@ def user_session(id):
         else:
             print("Please enter a valid Option #")
 
+
 def artist_session():
     print("Artist Session")
+
 
 def clearTerminal():
     os.system('cls' if os.name == 'nt' else 'clear')  # Clear the system terminal to look cleaner
 
-def main():
 
+def main():
     while True:
         userfound = False
         artistfound = False
@@ -118,10 +164,9 @@ def main():
             userfound = True
             foundUserName = data[0][0]
 
-
         cur.execute("select name from artists where aid=?", (id,))
         data = cur.fetchall()
-        
+
         if not data:
             artistfound = False
         else:
@@ -129,7 +174,7 @@ def main():
             foundArtistName = data[0][0]
 
         # If the ID exists in both users and artists
-        if (artistfound == True and userfound == True ):
+        if (artistfound == True and userfound == True):
             print("This ID exists for both a user and an artist")
             print("Press 1 to log in as the user {}: {}".format(id, foundUserName))
             print("Press 2 to log in as the artist {}: {}".format(id, foundArtistName))
@@ -141,19 +186,20 @@ def main():
                 print("\r", end="")
                 user_option = input("Invalid option. Please enter either 1 for user or 2 for artist: ")
 
-            userpass = getpass(prompt=("Please enter your password (as a" + (" user): " if user_option == "1" else "n artist): ")))
+            userpass = getpass(
+                prompt=("Please enter your password (as a" + (" user): " if user_option == "1" else "n artist): ")))
             cur.execute("select {} from {} where {}=? and pwd=?".format(
                 "uid" if user_option == "1" else "aid", "users" if user_option == "1" else "artists",
-                "uid" if user_option == "1" else "aid"), (id,userpass))
+                "uid" if user_option == "1" else "aid"), (id, userpass))
             data = cur.fetchall()
 
-            while(not data):
+            while (not data):
                 userpass = getpass(prompt="Incorrect password. Please try again: ")
                 cur.execute("select {} from {} where {}=? and pwd=?".format(
                     "uid" if user_option == "1" else "aid", "users" if user_option == "1" else "artists",
-                    "uid" if user_option == "1" else "aid"), (id,userpass))
+                    "uid" if user_option == "1" else "aid"), (id, userpass))
                 data = cur.fetchall()
-            
+
             print("Log-in Successful! Navigating to main screen...")
             time.sleep(0)
             clearTerminal()
@@ -163,12 +209,12 @@ def main():
         # If the id exists for only the user
         elif (userfound == True and artistfound == False):
             userpass = getpass(prompt="Please enter your password: ")
-            cur.execute("select uid from users where uid=? and pwd=?", (id,userpass))
+            cur.execute("select uid from users where uid=? and pwd=?", (id, userpass))
             data = cur.fetchall()
 
-            while(not data):
+            while (not data):
                 userpass = getpass(prompt="Incorrect password. Please try again: ")
-                cur.execute("select uid from users where uid=? and pwd=?", (id,userpass))
+                cur.execute("select uid from users where uid=? and pwd=?", (id, userpass))
                 data = cur.fetchall()
             else:
                 print("Log-in Successful! Navigating to main screen...")
@@ -179,20 +225,20 @@ def main():
         # If the id exists for only the artists
         elif (userfound == False and artistfound == True):
             userpass = getpass(prompt="Please Enter your Password: ")
-            cur.execute("select aid from artists where aid=? and pwd=?", (id,userpass))
+            cur.execute("select aid from artists where aid=? and pwd=?", (id, userpass))
             data = cur.fetchall()
 
-            while(not data):
+            while (not data):
                 userpass = getpass(prompt="Incorrect password. Please try again: ")
-                cur.execute("select aid from artists where aid=? and pwd=?", (id,userpass))
+                cur.execute("select aid from artists where aid=? and pwd=?", (id, userpass))
                 data = cur.fetchall()
             else:
                 print("Log-in Successful! Navigating to main screen...")
                 time.sleep(0)
                 clearTerminal()
-                user_session(id)   
+                user_session(id)
 
-        # Invalid Id. Ask for Sign-up
+                # Invalid Id. Ask for Sign-up
         else:
             print("No valid user or artist ID found. Would you like to sign-up as a new user?")
             user_option = input(str("Press 1 to continue!! "))
@@ -214,6 +260,8 @@ def main():
                     user_session(id)
 
                 else:
-                    print("This user-id already exists.")               
-            
-main()
+                    print("This user-id already exists.")
+
+
+# main()
+search_artists()
